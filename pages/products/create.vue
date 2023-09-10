81c05adf521d337da7ff1a60d10b202f3c22e4b8
hardow2011 <hardow2011@outlook.com>
2023-09-09 23:54:52 -0400

    feat: created component for product form

--- pages/products/create.vue
+++ pages/products/create.vue
@@ -1,27 +1,5 @@
 <template>
-  <form>
-    <label for="product_name">Name</label>
-    <input id="product_name" v-model="product.name">
-    <br>
-    <label for="product_description">Description</label>
-    <input id="product_description" v-model="product.description">
-    <br>
-    <label for="product_price">Price</label>
-    <input id="product_price" type="number" step="0.1" v-model="product.price">
-    <br>
-    <label for="product_quantity">Quantity</label>
-    <input id="product_quantity" type="number" step="1" v-model="product.quantity">
-    <br>
-    <label for="product_hidden">Hidden</label>
-    <input
-        id="product_hidden"
-        type="checkbox"
-        v-model="product.hidden"
-        true-value="true"
-        false-value="false" />
-    <br>
-    <button @click.prevent="createProduct">Create product</button>
-  </form>
+  <ProductForm :product="product" @publish-product="createProduct" />
 </template>
 
 <script>
