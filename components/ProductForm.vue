8be99db3b1f878ef4ef315f8fd054b819b88916a
hardow2011 <hardow2011@outlook.com>
2023-09-13 18:13:27 -0400

    feat: capture image fle in formdata

--- components/ProductForm.vue
+++ components/ProductForm.vue
@@ -3,7 +3,7 @@
     <label for="product_name">Name</label>
     <input id="product_name" v-model="product.name">
     <br>
-    <div v-for="product_option in product.product_options_attributes">
+    <div v-for="(product_option, index) in product.product_options_attributes">
       <label for="product_description">Description</label>
       <input id="product_description" v-model="product_option.description">
       <br>
@@ -13,7 +13,9 @@
       <label for="product_quantity">Quantity</label>
       <input id="product_quantity" type="number" step="1" v-model="product_option.quantity">
       <br>
-      <br>
+      <label for="product_media">Media</label>
+      <input id="product_media" type="file" ref="file" @change="uploadFile($event, index)" alt="">
+      {{ product_option }}
     </div>
     <label for="product_hidden">Hidden</label>
     <input
@@ -21,11 +23,23 @@
         type="checkbox"
         v-model="product.hidden"
         true-value="true"
-        false-value="false" />
+        false-value="false"/>
     <button @click.prevent="$emit('publishProduct')">Create product</button>
   </form>
 </template>
 
 <script setup>
+
   const { product } = defineProps(['product'])
+
+  function uploadFile(event, product_option_index) {
+    let formData = new FormData();
+    let file = event.target.files[0]
+
+    formData.append('media', file)
+    formData.append('order', 1)
+
+    product.product_options_attributes[product_option_index].image = formData
+  }
+
 </script>
