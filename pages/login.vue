17f087bb519c6eaaa07d366528d943b368d64bce
hardow2011 <hardow2011@outlook.com>
2023-09-21 22:41:46 -0400

    feat: login screen

---
+++ pages/login.vue
@@ -0,0 +1,27 @@
+<template>
+  <form>
+    <input type="email" v-model="user.email">
+    <button @click.prevent="login">Log in</button>
+  </form>
+</template>
+
+<script setup>
+
+  import {useUserStore} from "~/stores/user";
+
+  const userStore = useUserStore()
+  const user = userStore.user
+
+  function login() {
+    userStore.authenticateUser()
+  }
+
+  definePageMeta({
+    layout: false,
+  })
+
+</script>
+
+<style scoped>
+
+</style>
